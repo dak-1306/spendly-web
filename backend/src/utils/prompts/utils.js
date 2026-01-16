bf9c86c7fe5f418988d0665f11cccc7749f8e47c
dak-1306 <thd13062005@gmail.com>
2026-01-16 17:58:52 +0700

    chore: sử dụng functions của firebase

--- backend/src/utils/prompts/utils.js
+++
@@ -1,8 +0,0 @@
-import { systemPrompt } from "./system.prompt.js";
-
-export const buildPrompt = (userPrompt) => {
-  return [
-    { role: "system", content: systemPrompt },
-    { role: "user", content: userPrompt },
-  ];
-};
